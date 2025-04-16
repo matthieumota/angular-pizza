3a7d9b9d4ce0d4a76601b5ec11cfb8de4da974d4
Matthieu Mota <matthieumota@gmail.com>
2025-04-16 15:18:25 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Pizza } from './models/pizza';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+const PIZZAS: Pizza[] = [
+  { id: 1, name: 'Reine', price: 12, image: 'reine.jpg' },
+  { id: 2, name: '4 fromages', price: 13, image: '4-fromages.jpg' },
+  { id: 3, name: 'Orientale', price: 11, image: 'orientale.jpg' },
+  { id: 4, name: 'Cannibale', price: 9, image: 'cannibale.jpg' }
+];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [FormsModule, CommonModule],
 })
 export class AppComponent {
-  title = 'pizzaparty';
+  title = 'Pizza Party';
+  pizza!: Pizza;
+  pizzas: Pizza[] = PIZZAS;
+
+  onSelect(pizza: Pizza) {
+    this.pizza = { ...pizza }; // On fait un clone de l'objet pour éviter les "références"
+  }
 }
